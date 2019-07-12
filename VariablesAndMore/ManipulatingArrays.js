6cecc634ad4c02babc41726feb70a906c8867e5e
Sandra Benítez-Peña <45458926+sbenitez1@users.noreply.github.com>
2019-07-12 12:35:07 +0200

    Add files via upload

--- VariablesAndMore/ManipulatingArrays.js
+++ VariablesAndMore/ManipulatingArrays.js
@@ -22,8 +22,11 @@ myArray.unshift(0);         //The unshift method is used to insert a variable at
 console.log(myArray);       // will print out 0,1,2,3
 
 var myArray = [0,1,2,3,4,5,6,7,8,9];
+var myArray2 = [0,1,2,3,4,5,6,7,8,9];
 var splice = myArray.splice(3,5);   //At position 3 remove 5 elements
+var slice = myArray2.slice(3,7);   //Elements at position 3 to (7-1)=6 elements
 
 console.log(splice);        // will print out 3,4,5,6,7
 console.log(myArray);       // will print out 0,1,2,8,9
 
+console.log(slice);        // will print out 3,4,5,6
