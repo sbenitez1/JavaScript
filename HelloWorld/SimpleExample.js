edf9a8bf9319c5d092aaf995e246dc0e23ab404d
Sandra Benítez-Peña <45458926+sbenitez1@users.noreply.github.com>
2019-07-12 12:00:06 +0200

    Add files via upload

---
+++ HelloWorld/SimpleExample.js
@@ -0,0 +1,11 @@
+var myNumber = 3;
+var myString = "Hello, World!";
+var myBoolean = true;
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
